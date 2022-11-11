d164d04eaf8e93cdc2e534d0e7113041d050fe80
Grzesiek <grzesiekkedzior@gmail.com>
2022-11-11 19:49:32 +0100

    Add settings button
    Add the settings button and its menu. Change background colours.

--- app/src/main/java/com/weather/mapp/MainActivity.kt
+++ app/src/main/java/com/weather/mapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.weather.mapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var button: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popMenu()
+        supportActionBar?.hide();
+    }
+
+    fun popMenu() {
+        button = findViewById<View>(R.id.imageButtom) as ImageButton
+
+        button!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val popupMenu = PopupMenu(this@MainActivity, button)
+                popupMenu.menuInflater.inflate(R.menu.side_menu, popupMenu.menu)
+                popupMenu.show()
+            }
+        })
     }
 }
